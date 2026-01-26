667d7a5e6a12207a520b22eca408545392fee2a3
me-prakhargupta <prakhargupta089@gmail.com>
2026-01-26 20:24:07 +0530

    feat: add login and signup pages

---
+++ src/app/accounts/signup/page.tsx
@@ -0,0 +1,58 @@
+import AuthInput from "@/components/input/AuthInput";
+import Link from "next/link";
+
+export default function Home() {
+    return(
+        <div className="min-h-screen flex justify-center items-center px-4 py-10 bg-zinc-900">
+            <div className="w-full max-w-xl">
+                <header>
+                    <h1 className="font-bold text-2xl sm:text-3xl md:text-4xl mb-1">Get started with Confessly</h1>
+                    <p className="mb-2 text-gray-300 font-medium text-sm sm:text-base">Some thoughts deserve a safe place.</p>
+                    <p className="font-medium text-lg sm:text-xl md:text-2xl">Create your account and confess freely</p>
+                </header>
+
+                <main>
+                    <div className="mt-6">
+                        <p className="font-bold text-base sm:text-lg mb-2 sm:mb-3">Email address</p>
+
+                        <AuthInput type="text" name="email" lable="Email address"/>
+
+                        <p className="font-semibold text-sm sm:text-base text-gray-300 mt-1">You may receive a notification from us. <span className="text-orange-400 hover:text-orange-300"><Link href="/">Learn why we ask for verification</Link></span></p>
+                    </div>
+                    
+                    <div className="mt-6">
+                        <p className="font-bold text-base sm:text-lg mb-2 sm:mb-3">Password</p>
+                        <AuthInput type="password" name="password" lable="Password"/>
+                    </div>
+
+                    <div className="mt-6">
+                        <p className="font-bold text-base sm:text-lg mb-2 sm:mb-3">Name</p>
+                        <AuthInput type="text" name="fullname" lable="Full name"/>
+                    </div>
+
+                    <div className="mt-6">
+                        <p className="font-bold text-base sm:text-lg mb-2 sm:mb-3">Username</p>
+                        <AuthInput type="text" name="username" lable="Username"/>
+                        <p className="font-semibold text-base text-gray-300 mt-1">Username can contain only lowercase letters (a–z), numbers (0–9), and underscores (_).</p>
+                    </div>
+
+                    <div>
+                        <p className="mt-7 font-semibold text-sm sm:text-base leading-relaxed">By tapping Submit, you agree to create an account and to Confessly's <span className="text-orange-400 hover:text-orange-300"><Link href="/">Terms</Link></span>, <span className="text-orange-400 hover:text-orange-300"><Link href="/">Privacy Policy</Link></span>  and <span className="text-orange-400 hover:text-orange-300"><Link href="/"> Cookies Policy</Link></span>.</p>
+
+                        <p className="mt-5 font-semibold">We use your details only to create, verify, and secure your account.</p>
+                    </div>
+
+                    <div className="mt-6">
+                        <button className="w-full border border-orange-400 text-black bg-orange-400 hover:bg-orange-300 hover:border-orange-300 text-base sm:text-lg font-semibold py-3 sm:py-4 rounded-full hover:cursor-pointer">Submit</button>
+                    </div>
+                </main>
+
+                <section>
+                    <div className="flex justify-center mt-10">
+                        <Link href="/accounts/signin" className="inline-block w-full text-center font-semibold border border-white/70 py-3 sm:py-4 rounded-full hover:cursor-pointer hover:bg-zinc-950 text-sm sm:text-base">I already have an account</Link>
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/input/AuthInput.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+interface AuthFormProps {
+  type?: string;
+  lable: string;
+  name: string;
+}
+
+export default function FloatingInput({ type, lable, name }: AuthFormProps) {
+  return (
+    <div className="relative w-full">
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder=" "
+        className="
+          peer block w-full rounded-2xl outline-none border bg-zinc-950
+          h-14 sm:h-16
+          px-4 pt-5 sm:pt-6
+          text-base sm:text-lg
+          focus:border-orange-400 focus:ring-2 focus:ring-orange-500/30
+        "
+      />
+
+      <label
+        htmlFor={name}
+        className="
+          absolute left-4 top-4 text-white/50 transition-all duration-200
+          text-sm sm:text-base
+
+          peer-placeholder-shown:top-4
+          peer-placeholder-shown:text-base sm:peer-placeholder-shown:text-lg
+          peer-placeholder-shown:text-white/40
+
+          peer-focus:top-2
+          peer-focus:text-xs sm:peer-focus:text-sm
+          peer-focus:text-orange-400
+
+          peer-not-placeholder-shown:top-2
+          peer-not-placeholder-shown:text-xs sm:peer-not-placeholder-shown:text-sm
+          peer-not-placeholder-shown:text-white/50
+        "
+      >
+        {lable}
+      </label>
+    </div>
+  );
+}
